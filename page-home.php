68cc9b969b5fcfe8da7ec4febf4d2f55190b767b
Ryan Closner <ryan@ryanclosner.com>
2012-11-17 18:11:31 -0800

    Add tagline to home page

--- page-home.php
+++ page-home.php
@@ -7,9 +7,13 @@ Template Name: Home
 <?php get_header(); ?>
 
 <section id="content" role="main">
-  <div id="tagline" class="hero-unit">
-    <h1>Bridge City Lacrosse Home Page</h1>
-  </div>
+  <?php frack_get_option_tree( 'tagline', function ($tagline) {
+    ?>
+      <div id="tagline" class="hero-unit">
+      <h1><?php echo $tagline; ?></h1>
+      </div>
+    <?php
+  }); ?>
 
   <div id="content-wrapper" class="container-fluid">
     <?php get_sidebar( "home" ); ?>
